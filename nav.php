e545f6e7614cdb181ab9d48f25286a52227efbed
Fachrulrozy099 <fachrulrozy099@gmail.com>
2024-10-14 15:58:30 +0700

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,13 @@
+<?php
+echo '<nav id="product-nav" class="kreon-regular" style ="background-color: black">';
+echo '<h1 id="web-name" style="color:gold;">Toko Emas Cantik New</h1>';
+echo '<div id="feature">';
+echo '<h3 id="filter">Filter</h3>';
+echo '<a href="public/login.html" id="login" style="color:white;text-decoration:none;">Login</a>';
+echo '<button id="card-button">';
+echo '<svg xmlns="http://www.w3.org/2000/svg" width="1.4em" height="1.4em" viewBox="0 0 24 24"><path fill="currentColor" d="M17 18c-1.11 0-2 .89-2 2a2 2 0 0 0 2 2a2 2 0 0 0 2-2a2 2 0 0 0-2-2M1 2v2h2l3.6 7.59l-1.36 2.45c-.15.28-.24.61-.24.96a2 2 0 0 0 2 2h12v-2H7.42a.25.25 0 0 1-.25-.25q0-.075.03-.12L8.1 13h7.45c.75 0 1.41-.42 1.75-1.03l3.58-6.47c.07-.16.12-.33.12-.5a1 1 0 0 0-1-1H5.21l-.94-2M7 18c-1.11 0-2 .89-2 2a2 2 0 0 0 2 2a2 2 0 0 0 2-2a2 2 0 0 0-2-2"/></svg>';
+echo '<span>Cart</span>';
+echo '</button>';
+echo '</div>';
+echo '</nav>';
+?>
